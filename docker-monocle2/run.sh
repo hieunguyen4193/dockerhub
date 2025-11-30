85fb24875a67f309a0ae4a38e1a6c5aa5bf944db
Trong-Hieu Nguyen <work@hieunguyen.de>
2025-11-30 16:11:18 +0100

    update monocle 2.12 - r3.6 docker and rna velocity docker

--- docker-monocle2/run.sh
+++ docker-monocle2/run.sh
@@ -1,7 +1,10 @@
 # docker buildx create --use --name docker_single_cell_pipeline_builder
 # docker buildx inspect --bootstrap
-docker buildx use docker_single_cell_pipeline_builder
+# docker buildx use docker_single_cell_pipeline_builder
 docker buildx build \
     --platform linux/amd64,linux/arm64 \
     -t tronghieunguyen/monocle212_r36 \
     --push . 
+
+# since the docker with r 3.6 is quite old, we just build as default platform.
+# docker build -t tronghieunguyen/monocle212_r36 .
