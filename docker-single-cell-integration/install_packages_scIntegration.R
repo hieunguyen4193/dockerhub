94a58349f1466f8eea68a9273a018cffc052ae31
Trong-Hieu Nguyen <work@hieunguyen.de>
2025-12-05 16:54:07 +0100

    update dockerfile for single cell integration packages

---
+++ docker-single-cell-integration/install_packages_scIntegration.R
@@ -0,0 +1,8 @@
+# install fastMNN: Fast mutual nearest neighbors correction
+remotes::install_github("LTLA/batchelor")
+
+# install harmony
+install.packages("harmony")
+
+# install LIGER
+install.packages("rliger")
